9d0c12c469fdd938cada5e8ac1064568bd285277
Krunal Shah <krunalshahmo@yahoo.com>
2014-02-20 20:12:07 -0600

    Resizing table rows, based on screen size

--- src/com/example/safecommute/MainActivity.java
+++ src/com/example/safecommute/MainActivity.java
@@ -6,6 +6,8 @@ package com.example.safecommute;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.TableRow;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +17,35 @@ public class MainActivity extends Activity {
 		// going to be the fragment container for the main view
 		setContentView(R.layout.activity_main); // main program
 		//setContentView(R.layout.alpha_clickable_title); // Jakob Test
+		
+		// Resizing table rows, based on screen size
+		Display display = getWindowManager().getDefaultDisplay(); 
+		int screenWidth = display.getWidth();  // screen width
+		int screenHeight = display.getHeight();  // screen height	
+		
+		// Dynamically adding rows in the Table
+		TableRow titlerow = (TableRow)findViewById(R.id.titlerow); 
+		titlerow.setMinimumHeight(screenHeight/4);
+		
+		TableRow appstextrow = (TableRow)findViewById(R.id.appstextrow); 
+		appstextrow.setMinimumHeight(screenHeight/20);
+		
+		TableRow appsimagerow = (TableRow)findViewById(R.id.appsimagerow); 
+		appsimagerow.setMinimumHeight(screenHeight/5);
+		
+		TableRow passengertextrow = (TableRow)findViewById(R.id.passengertextrow); 
+		passengertextrow.setMinimumHeight(screenHeight/20);
+		
+		TableRow passengerimagerow = (TableRow)findViewById(R.id.passengerimagerow); 
+		passengerimagerow.setMinimumHeight(screenHeight/5);
+		
+		TableRow emergencytextrow = (TableRow)findViewById(R.id.emergencytextrow); 
+		emergencytextrow.setMinimumHeight(screenHeight/20);
+		
+		TableRow emergencyimagerow = (TableRow)findViewById(R.id.emergencyimagerow); 
+		emergencyimagerow.setMinimumHeight(screenHeight/5); 
+		
+		
 	}
 
 	//	@Override
