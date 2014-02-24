5eea503e1f72c7c91c530ccadf239c0c7059665d
Krunal Shah <krunalshahmo@yahoo.com>
2014-02-23 23:06:05 -0600

    Dynamically resizing text views

--- src/com/example/safecommute/MainActivity.java
+++ src/com/example/safecommute/MainActivity.java
@@ -6,10 +6,13 @@
 package com.example.safecommute;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -19,6 +22,7 @@ public class MainActivity extends Activity {
 		// going to be the fragment container for the main view
 		setContentView(R.layout.activity_main); // UI Lock-Screen Layout
 		scalingImages(); // Dynamically size images, according to screen size	
+		scalingTexts();	
 		
 		//setContentView(R.layout.alpha_clickable_title); // Jakob's Test	
 	}
@@ -61,15 +65,12 @@ public class MainActivity extends Activity {
 		int screenHeight = display.getHeight();  // screen height			
 	
 		// Assigning table rows
-		TableRow titlerow = (TableRow)findViewById(R.id.titlerow); 		
-		//TableRow appstextrow = (TableRow)findViewById(R.id.appstextrow); 
-		TableRow appsimagerow = (TableRow)findViewById(R.id.appsimagerow); 
-		//TableRow passengertextrow = (TableRow)findViewById(R.id.passengertextrow); 
+		TableRow titlerow = (TableRow)findViewById(R.id.titlerow);
+		TableRow appsimagerow = (TableRow)findViewById(R.id.appsimagerow);
 		TableRow passengerimagerow = (TableRow)findViewById(R.id.passengerimagerow); 
-		//TableRow emergencytextrow = (TableRow)findViewById(R.id.emergencytextrow); 
 		TableRow emergencyimagerow = (TableRow)findViewById(R.id.emergencyimagerow); 
 		
-		// Scaled Dimensions
+		// New Dimensions
 		int imageHeight = screenHeight/5;
 		int textHeight = screenHeight/20;
 		int titleHeight = screenHeight/5;
@@ -79,9 +80,6 @@ public class MainActivity extends Activity {
 		TableRow.LayoutParams titleParams = new TableRow.LayoutParams(
 			    titleWidth, titleHeight);
 		titleParams.span = 3;
-		TableRow.LayoutParams textParams = new TableRow.LayoutParams(
-				screenWidth, textHeight); // currently unused
-		textParams.span = 3;
 		TableRow.LayoutParams imageParams = new TableRow.LayoutParams(
 			    imageHeight, imageHeight);
 
@@ -92,21 +90,10 @@ public class MainActivity extends Activity {
 		title.setScaleType(ImageView.ScaleType.FIT_CENTER);		
 		titlerow.addView(title);
 		
-        /*// Adding "Available Applications" text, may need to use for scaling texts		
-		TextView availableappstext = new TextView(this);
-		availableappstext.setText(R.string.available_apps);
-		availableappstext.setBackgroundResource(R.layout.red1);
-		availableappstext.setTextColor(Color.WHITE);
-		availableappstext.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
-		availableappstext.setLayoutParams(textParams);
-		appstextrow.addView(availableappstext);*/
-		//var myfontsize = (Titanium.Platform.displayCaps.platformHeight * 3) / 100;
-		
 		// Adding images to Available Apps row		
 		ImageView settingsimage = new ImageView(this);
 		settingsimage.setImageResource(R.drawable.settings);
 		settingsimage.setLayoutParams(imageParams);
-		//settingsimage.setScaleType(ImageView.ScaleType.FIT_CENTER);
 		appsimagerow.addView(settingsimage);
 		
 		ImageView musicimage = new ImageView(this);
@@ -135,6 +122,51 @@ public class MainActivity extends Activity {
 		ImageView emergencyimage = new ImageView(this);
 		emergencyimage.setImageResource(R.drawable.emergency);
 		emergencyimage.setLayoutParams(imageParams);
-		emergencyimagerow.addView(emergencyimage);			
+		emergencyimagerow.addView(emergencyimage);				
+	}
+	
+	public void scalingTexts(){
+		
+		Display display = getWindowManager().getDefaultDisplay(); 
+		int screenWidth = display.getWidth();  // screen width
+		int screenHeight = display.getHeight();  // screen height
+		int textHeight = screenHeight/20; // new text height
+		
+		// Assigning table rows
+		TableRow appstextrow = (TableRow)findViewById(R.id.appstextrow); 
+		TableRow passengertextrow = (TableRow)findViewById(R.id.passengertextrow); 
+		TableRow emergencytextrow = (TableRow)findViewById(R.id.emergencytextrow); 
+		
+		// Layout parameters for text views
+		TableRow.LayoutParams textParams = new TableRow.LayoutParams(
+				screenWidth, textHeight); // 
+		textParams.span = 3;
+		
+		// Format and add "Passenger Unlock" text
+		TextView passengertext = new TextView(this);
+		passengertext.setText(R.string.passenger_unlock);
+		passengertext.setLayoutParams(textParams);
+		passengertext.setGravity(Gravity.CENTER);
+		passengertext.setTextColor(Color.WHITE);
+		passengertext.setTextSize(16 * getResources().getDisplayMetrics().density);		
+		passengertextrow.addView(passengertext);
+		
+		// Format and add "Available Applications" text
+		TextView appstext = new TextView(this);
+		appstext.setText(R.string.available_apps);
+		appstext.setLayoutParams(textParams);
+		appstext.setGravity(Gravity.CENTER);
+		appstext.setTextColor(Color.WHITE);
+		appstext.setTextSize(16 * getResources().getDisplayMetrics().density);		
+		appstextrow.addView(appstext);
+		
+		// Format and add "Emergency" text
+		TextView emergencytext = new TextView(this);
+		emergencytext.setText(R.string.emergency_contacts);
+		emergencytext.setLayoutParams(textParams);
+		emergencytext.setGravity(Gravity.CENTER);
+		emergencytext.setTextColor(Color.WHITE);
+		emergencytext.setTextSize(16 * getResources().getDisplayMetrics().density);		
+		emergencytextrow.addView(emergencytext);		
 	}
 }
