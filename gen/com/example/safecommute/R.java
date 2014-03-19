1d27dcf9c3c195e9806d9b926279b24f94f4cb00
mtabor150 <mtabor150@gmail.com>
2014-03-19 15:23:27 -0500

    merging

--- gen/com/example/safecommute/R.java
+++ gen/com/example/safecommute/R.java
@@ -11,21 +11,48 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bluetooth=0x7f020000;
+        public static final int camera=0x7f020001;
+        public static final int emergency=0x7f020002;
+        public static final int gmaps=0x7f020003;
+        public static final int ic_safecommute=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int music=0x7f020006;
+        public static final int settings=0x7f020007;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int appsimagerow=0x7f070002;
+        public static final int appstextrow=0x7f070001;
+        public static final int emergencyimagerow=0x7f070006;
+        public static final int emergencytextrow=0x7f070005;
+        public static final int hey=0x7f070008;
+        public static final int menu_settings=0x7f070009;
+        public static final int passengerimagerow=0x7f070004;
+        public static final int passengertextrow=0x7f070003;
+        public static final int titlerow=0x7f070000;
+        public static final int titles=0x7f070007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int alpha_clickable_display=0x7f030001;
+        public static final int alpha_clickable_title=0x7f030002;
+        public static final int green1=0x7f030003;
+        public static final int green2=0x7f030004;
+        public static final int red1=0x7f030005;
+        public static final int red2=0x7f030006;
+        public static final int yellow1=0x7f030007;
+        public static final int yellow2=0x7f030008;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
+        public static final int available_apps=0x7f040003;
+        public static final int emergency_contacts=0x7f040005;
         public static final int menu_settings=0x7f040001;
+        public static final int passenger_unlock=0x7f040004;
+        public static final int phone_in_motion=0x7f040002;
     }
     public static final class style {
         /** 
@@ -54,5 +81,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
+        public static final int CustomTheme=0x7f050002;
     }
 }
