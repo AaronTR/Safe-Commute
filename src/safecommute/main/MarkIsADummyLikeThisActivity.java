6d92bc8cdde6d1c53cc142387bfb52823ded0355
jwisch91 <jwisch1@slu.edu>
2014-05-02 18:02:26 -0500

    Added Boot files

---
+++ src/safecommute/main/MarkIsADummyLikeThisActivity.java
@@ -0,0 +1,31 @@
+package safecommute.main;
+
+import safecommute.bluetooth.BluetoothActivity;
+import safecommute.movement.GPSService;
+import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MarkIsADummyLikeThisActivity extends Activity{
+	
+		private Intent LocIntent = null;
+		public static GPSService loc = null;
+		public static Service newService;
+		
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);		
+			
+			Toast.makeText(this, "DUMMY WORKED F YEAH!", Toast.LENGTH_SHORT).show();
+			
+			LocIntent = new Intent(this, GPSService.class);
+			startService(LocIntent);
+			loc = new GPSService(this);
+			
+			finish();
+				
+		}
+
+}
