d7262233898525cc68c5555a15db2026653abda5
JakobKlinger <jklinge3@slu.edu>
2014-05-02 17:28:10 -0500

    DumClass addition, start from service, no longer an app.

--- src/safecommute/main/Maps.java
+++ src/safecommute/main/Maps.java
@@ -5,17 +5,10 @@ import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.Display;
-import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
-import android.widget.LinearLayout;
-import android.widget.TableRow;
 
 public class Maps extends Activity implements LocationListener{
 	LocationManager locationManager;	
@@ -26,33 +19,6 @@ public class Maps extends Activity implements LocationListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maps);
-        
-        
-        // Add logo to the top
-        Display display = getWindowManager().getDefaultDisplay(); 
-		int screenWidth = display.getWidth();  // screen width
-		int screenHeight = display.getHeight();  // screen height	
-		int titleHeight = screenHeight/5;
-		int titleWidth = screenWidth/4;
-		
-		
-
-		ImageView titlerow = (ImageView)findViewById(R.id.logo);
-		titlerow.setImageResource(R.drawable.logo);
-		titlerow.getLayoutParams().height = titleHeight;
-		titlerow.getLayoutParams().width = titleWidth;
-		titlerow.setScaleType(ScaleType.FIT_XY);
-		
-		/*ImageView imageView = (ImageView)findViewById(R.id.logo);
-		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
-		bitmap = Bitmap.createScaledBitmap(bitmap, titleWidth, titleHeight, true);
-		imageView.setImageBitmap(bitmap);*/
-	
-
-
-        
-        
-        
         
         locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
         Criteria c = new Criteria();
